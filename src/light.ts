06e547230a69b893224ee18fa43487baad5621c7
Julien Pelletier <julien.pelletier8@gmail.com>
2023-04-14 10:16:09 +0200

    feat: one gpio pin only for light

--- src/light.ts
+++ src/light.ts
@@ -1,43 +1,30 @@
 import * as os from "os";
 import { Gpio } from "pigpio";
-import myEE from "./event-handler";
-import { LightParams } from "./types";
+import eventHandler from "./event-handler";
 
 const arch = os.arch();
 class Light {
-	bluePin;
-	redPin;
-	greenPin;
+	lightPin;
 	lightEnabled: boolean;
 
 	constructor(lightEnabled = true) {
 		this.lightEnabled = lightEnabled;
 		if (arch.includes("arm") && lightEnabled) {
-			this.bluePin = new Gpio(+process.env.BLUE_GPIO_PIN, {
-				mode: Gpio.OUTPUT
-			});
-			this.redPin = new Gpio(+process.env.RED_GPIO_PIN, {
-				mode: Gpio.OUTPUT
-			});
-			this.greenPin = new Gpio(+process.env.GREEN_GPIO_PIN, {
+			this.lightPin = new Gpio(+process.env.LIGHT_GPIO_PIN, {
 				mode: Gpio.OUTPUT
 			});
 		}
-		myEE.on("light:on", this.turnOn.bind(this));
-		myEE.on("light:off", this.turnOff.bind(this));
+		eventHandler.on("light:on", this.turnOn.bind(this));
+		eventHandler.on("light:off", this.turnOff.bind(this));
 	}
 	turnOn() {
 		if (arch.includes("arm") && this.lightEnabled) {
-			this.bluePin.digitalWrite(1);
-			this.redPin.digitalWrite(1);
-			this.greenPin.digitalWrite(1);
+			this.lightPin.digitalWrite(1);
 		}
 	}
 	turnOff() {
 		if (arch.includes("arm") && this.lightEnabled) {
-			this.bluePin.digitalWrite(0);
-			this.redPin.digitalWrite(0);
-			this.greenPin.digitalWrite(0);
+			this.lightPin.digitalWrite(0);
 		}
 	}
 }
